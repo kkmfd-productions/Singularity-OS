b592a1ca8a7fb7ce88a5600a79bb2d13a0721cd7
KKMFD <100986647+kkmfd-Offical@users.noreply.github.com>
2024-01-22 19:11:59 -0800

    Add files via upload

---
+++ Singularity OS/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Singularity_OS
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            label4 = new Label();
+            label3 = new Label();
+            tabPage2 = new TabPage();
+            label2 = new Label();
+            button1 = new Button();
+            label1 = new Label();
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            toolStripProgressBar1 = new ToolStripProgressBar();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            statusStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Appearance = TabAppearance.FlatButtons;
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.HotTrack = true;
+            tabControl1.Location = new Point(1, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(1302, 674);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.BackColor = SystemColors.GradientActiveCaption;
+            tabPage1.BackgroundImage = Properties.Resources.icon;
+            tabPage1.BackgroundImageLayout = ImageLayout.Center;
+            tabPage1.Controls.Add(label4);
+            tabPage1.Controls.Add(label3);
+            tabPage1.Location = new Point(4, 27);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(1294, 643);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Home";
+            tabPage1.Click += tabPage1_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Ubuntu Mono", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(421, 463);
+            label4.Name = "label4";
+            label4.Size = new Size(468, 29);
+            label4.TabIndex = 3;
+            label4.Text = "Singularity OS V0.1 Developer Build\r\n";
+            label4.Click += label4_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Ubuntu Mono", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(606, 12);
+            label3.Name = "label3";
+            label3.Size = new Size(73, 30);
+            label3.TabIndex = 0;
+            label3.Text = "Home";
+            // 
+            // tabPage2
+            // 
+            tabPage2.BackColor = SystemColors.GradientActiveCaption;
+            tabPage2.BackgroundImage = Properties.Resources.icon;
+            tabPage2.BackgroundImageLayout = ImageLayout.Center;
+            tabPage2.Controls.Add(label2);
+            tabPage2.Controls.Add(button1);
+            tabPage2.Controls.Add(label1);
+            tabPage2.Location = new Point(4, 27);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(1294, 643);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "System";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Ubuntu Mono", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(820, 3);
+            label2.Name = "label2";
+            label2.Size = new Size(468, 29);
+            label2.TabIndex = 2;
+            label2.Text = "Singularity OS V0.1 Developer Build\r\n";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Ubuntu Mono", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(7, 54);
+            button1.Name = "button1";
+            button1.Size = new Size(228, 34);
+            button1.TabIndex = 1;
+            button1.Text = "Software Update";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(7, 14);
+            label1.Name = "label1";
+            label1.Size = new Size(228, 37);
+            label1.TabIndex = 0;
+            label1.Text = "System Prefrences";
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1, toolStripProgressBar1 });
+            statusStrip1.Location = new Point(0, 649);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(1303, 22);
+            statusStrip1.TabIndex = 1;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(129, 17);
+            toolStripStatusLabel1.Text = "Singularity OS Progress";
+            // 
+            // toolStripProgressBar1
+            // 
+            toolStripProgressBar1.Name = "toolStripProgressBar1";
+            toolStripProgressBar1.Size = new Size(100, 16);
+            toolStripProgressBar1.ToolTipText = "Project Build Status";
+            toolStripProgressBar1.Value = 4;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.GradientActiveCaption;
+            BackgroundImage = Properties.Resources.icon;
+            BackgroundImageLayout = ImageLayout.Center;
+            ClientSize = new Size(1303, 671);
+            Controls.Add(statusStrip1);
+            Controls.Add(tabControl1);
+            DoubleBuffered = true;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Singularity";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private Label label2;
+        private Button button1;
+        private Label label1;
+        private Label label3;
+        private Label label4;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        private ToolStripProgressBar toolStripProgressBar1;
+    }
+}
---
+++ Singularity OS/Form1.cs
@@ -0,0 +1,25 @@
+namespace Singularity_OS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Software Update Is Not available For Developer Builds Or Pre Release Builds", "Singularity OS", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
